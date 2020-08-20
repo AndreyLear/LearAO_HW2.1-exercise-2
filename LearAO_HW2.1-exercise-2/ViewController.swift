5f4834e0bda3253342830bca0f15e835bb41454b
Andrey Lear <learandrey@gmail.com>
2020-08-20 21:34:36 +1000

    Initial Commit

---
+++ LearAO_HW2.1-exercise-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearAO_HW2.1-exercise-2
+//
+//  Created by Andrey Lear on 20.08.2020.
+//  Copyright © 2020 Andrey Lear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
